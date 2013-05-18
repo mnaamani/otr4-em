8b4921d178b963f8b56c7115fc28604146a1f2ec
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2013-05-18 12:05:58 +0300

    don't allocate() STATIC in preRun

--- src/otr_pre.js
+++ src/otr_pre.js
@@ -44,9 +44,9 @@ Module['preRun'].push(function(){
       return Math.floor(Math.random() * 256);
     }));
     
-    _static_buffer_ptr = allocate(4096,"i8",ALLOC_STATIC); 
-    _static_new_mpi_ptr_ptr = allocate(4,"i8",ALLOC_STATIC);
-    _static_otr_error_message_str = allocate(512,"i8",ALLOC_STATIC);
+    _static_buffer_ptr = allocate(4096,"i8",ALLOC_NORMAL);
+    _static_new_mpi_ptr_ptr = allocate(4,"i8",ALLOC_NORMAL);
+    _static_otr_error_message_str = allocate(512,"i8",ALLOC_NORMAL);
 
     Module["libgcrypt"] = {};
     Module["libgcrypt"]["mpi_new"] = gcry_.mpi_new = cwrap('_gcry_mpi_new','number',['number']);
